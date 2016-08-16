d29086f5c3050962aebb2888c430545dcaedffb7
devmrko <devmrko@gmail.com>
2016-08-16 13:17:47 +0900

    apply db table columns changes

--- routes/job.js
+++ routes/job.js
@@ -4,56 +4,33 @@ var perPage = 5;
 var url = require('url');
 var async = require('async');
 var ObjectID = require('mongodb').ObjectID;
-
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host    :'192.168.10.33',
-    port : 3306,
-    user : 'hadoop',
-    password : 'hadoop',
-    database:'master'
-});
-
-connection.connect(function(err) {
-    if (err) {
-        console.error('mysql connection error');
-        console.error(err);
-        throw err;
-    }
-    console.log("Connected to mysql server");	
-});
-
 //select all
-router.post('/selectType', function(req,res){
+router.post('/selectType', function (req, res) {
     var searchword = req.body.searchword
-    //var query = "select * from stopwords where name like concat('%', '" + searchword + "', '%')"
+        //var query = "select * from stopwords where name like concat('%', '" + searchword + "', '%')"
     var query = "select type from chartExample";
     console.log("/route.post /selectType ");
-    var query = connection.query(query,function(err,rows){
-       // console.log(rows);
-	res.jsonp({
-                  "rows": rows
-               });
-
+    var query = connection.query(query, function (err, rows) {
+        // console.log(rows);
+        res.jsonp({
+            "rows": rows
+        });
     });
     console.log(query);
 });
-
-router.post('/graph', function(req,res){
+router.post('/graph', function (req, res) {
     var searchword = req.body.searchword
     console.log("post graph!");
     var query = "select * from chartExample where type='normal'";
     //var query = "select * from chartExample where type= eelike concat('%', '" + searchword + "', '%')"
-    var query = connection.query(query,function(err,rows){
-       // console.log(rows);
-	res.jsonp({
-                  "rows": rows
-               });
-
+    var query = connection.query(query, function (err, rows) {
+        // console.log(rows);
+        res.jsonp({
+            "rows": rows
+        });
     });
     console.log(query);
 });
-
 router.get('/', ensureAuthenticated, function (req, res, next) {
     res.render('job', {
         "title": 'job'
@@ -61,82 +38,71 @@ router.get('/', ensureAuthenticated, function (req, res, next) {
 });
 
 function ensureAuthenticated(req, res, next) {
-  if(req.isAuthenticated()) {
-    return next();
-  }
-  res.redirect('/users/login');
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/users/login');
 }
-
 router.post('/complete', ensureAuthenticated, function (req, res, next) {
     var selId = req.body.sel_id;
-
     var db = req.db;
     var test_cols = db.get('memo');
     test_cols.update({
-            "_id": selId
-        },
-            {
-                $set: {
-                    "complete": true
-                }
-            },
-            function (err, doc) {
-                if (err) {
-                    throw err;
-                } else {
-                    req.flash('success', 'post completed');
-                    res.jsonp({
-                                    "error_code": 0
-                                });
-                    // res.location('/memo');
-                    // res.redirect('/memo');
-                    // searchHandler(req, res, next);
-                }
-            }
-            );
-
+        "_id": selId
+    }, {
+        $set: {
+            "complete": true
+        }
+    }, function (err, doc) {
+        if (err) {
+            throw err;
+        }
+        else {
+            req.flash('success', 'post completed');
+            res.jsonp({
+                "error_code": 0
+            });
+            // res.location('/memo');
+            // res.redirect('/memo');
+            // searchHandler(req, res, next);
+        }
+    });
 });
-
 router.post('/cancelComplete', ensureAuthenticated, function (req, res, next) {
     var selId = req.body.sel_id;
-
     var db = req.db;
     var test_cols = db.get('contents');
     test_cols.update({
-            "_id": selId
-        },
-            {
-                $set: {
-                    "complete": false
-                }
-            },
-            function (err, doc) {
-                if (err) {
-                    throw err;
-                } else {
-                    req.flash('success', 'Cancel post completion');
-                    res.jsonp({
-                                    "error_code": 0
-                                });
-                    // res.location('/memo');
-                    // res.redirect('/memo');
-                    // searchHandler(req, res, next);
-                }
-            }
-            );
+        "_id": selId
+    }, {
+        $set: {
+            "complete": false
+        }
+    }, function (err, doc) {
+        if (err) {
+            throw err;
+        }
+        else {
+            req.flash('success', 'Cancel post completion');
+            res.jsonp({
+                "error_code": 0
+            });
+            // res.location('/memo');
+            // res.redirect('/memo');
+            // searchHandler(req, res, next);
+        }
+    });
 });
 
 function doJsonSearch(req, res, searchText, searchTags, curPage, completeBool) {
     var db = req.db;
     var mysqlConn = req.mysqlConn;
-    
     var query = "select * from jobMaster";
-    mysqlConn.query(query,function(err,rows){
-    res.jsonp({
-                  "resultDo": rows
-               });
+    mysqlConn.query(query, function (err, rows) {
+        res.jsonp({
+            "resultDo": rows
+        });
     });
-    
     // var test_cols = db.get('contents');
     // var searchQeury;
     // if(searchTags != 'All') {
@@ -154,7 +120,6 @@ function doJsonSearch(req, res, searchText, searchTags, curPage, completeBool) {
     // }
     // // var searchFields = {_id: 1, tags: 1, title: 1, edit_date: 1, contents: 0, reg_date: 0, complete: 0, due_date: 0, notice_bool: 0, reg_id: 0};
     // var searchFields = {contents: 0, reg_date: 0, complete: 0, due_date: 0, notice_bool: 0, reg_id: 0};
-
     // async.parallel([
     //     function(callback) {
     //         test_cols.distinct('tags', {'reg_id': req.user.name}, function (err, categories) {
@@ -182,25 +147,21 @@ function doJsonSearch(req, res, searchText, searchTags, curPage, completeBool) {
     //                     'total_cnt': results[2]
     //                 });
     // });
-
 }
-
 router.post('/search', ensureAuthenticated, function (req, res, next) {
     searchHandler(req, res, next);
 });
-
 router.post('/searchDetail', ensureAuthenticated, function (req, res, next) {
-  var db = req.db;
-  var test_cols = db.get('contents');
-  var selId = new ObjectID(req.body.sel_id);
-
-  test_cols.find({_id: selId},
-      function (err, test_cols) {
-          res.jsonp({
-                          "detailObj": test_cols
-                      });
-      });
-
+    var db = req.db;
+    var test_cols = db.get('contents');
+    var selId = new ObjectID(req.body.sel_id);
+    test_cols.find({
+        _id: selId
+    }, function (err, test_cols) {
+        res.jsonp({
+            "detailObj": test_cols
+        });
+    });
 });
 
 function searchHandler(req, res, next) {
@@ -210,35 +171,32 @@ function searchHandler(req, res, next) {
     var completeBool = req.body.completeBool === undefined ? false : req.body.completeBool;
     doJsonSearch(req, res, searchText, searchTags, curPage, completeBool);
 }
-
 router.post('/savePost', ensureAuthenticated, function (req, res, next) {
     // get form values
     var selContents = req.body.sel_contents;
     var selTitle = req.body.sel_title;
     var selTags = req.body.sel_tags;
     var selId = req.body.sel_id;
-
     var selDueDate = req.body.sel_due_date;
     var selNoticeBool = req.body.sel_notice_bool;
-
     var db = req.db;
     var test_cols = db.get('contents');
-
     if (selId == '') {
         test_cols.insert({
-            "contents": selContents,
-            "title": selTitle,
-            "tags": selTags,
-            "reg_date": new Date(),
-            "edit_date": new Date(),
-            "due_date": selDueDate,
-            "notice_bool": selNoticeBool,
-            "complete" : "n",
-            'reg_id': req.user.name
+            "contents": selContents
+            , "title": selTitle
+            , "tags": selTags
+            , "reg_date": new Date()
+            , "edit_date": new Date()
+            , "due_date": selDueDate
+            , "notice_bool": selNoticeBool
+            , "complete": "n"
+            , 'reg_id': req.user.name
         }, function (err, test_cols) {
             if (err) {
                 res.send('There was an issue submitting the post');
-            } else {
+            }
+            else {
                 // req.flash('success', 'Post Submitted');
                 // res.jsonp({
                 //                 "error_code": 0
@@ -248,38 +206,36 @@ router.post('/savePost', ensureAuthenticated, function (req, res, next) {
                 searchHandler(req, res, next);
             }
         });
-    } else {
+    }
+    else {
         test_cols.update({
             "_id": selId
-        },
-            {
-                $set: {
-                    "contents": selContents,
-                    "title": selTitle,
-                    "tags": selTags,
-                    "reg_date": new Date(),
-                    "edit_date": new Date(),
-                    "due_date": selDueDate,
-                    "notice_bool": selNoticeBool
-                }
-            },
-            function (err, doc) {
-                if (err) {
-                    throw err;
-                } else {
-                    //req.flash('success', 'Comment Added');
-                    // res.jsonp({
-                    //                 "error_code": 0
-                    //             });
-                    // // res.location('/posts/show/'+selId);
-                    // // res.redirect('/posts/show/'+selId);
-                    // res.location('/memo');
-                    // res.redirect('/memo');
-                    searchHandler(req, res, next);
-                }
+        }, {
+            $set: {
+                "contents": selContents
+                , "title": selTitle
+                , "tags": selTags
+                , "reg_date": new Date()
+                , "edit_date": new Date()
+                , "due_date": selDueDate
+                , "notice_bool": selNoticeBool
             }
-            );
+        }, function (err, doc) {
+            if (err) {
+                throw err;
+            }
+            else {
+                //req.flash('success', 'Comment Added');
+                // res.jsonp({
+                //                 "error_code": 0
+                //             });
+                // // res.location('/posts/show/'+selId);
+                // // res.redirect('/posts/show/'+selId);
+                // res.location('/memo');
+                // res.redirect('/memo');
+                searchHandler(req, res, next);
+            }
+        });
     }
 });
-
 module.exports = router;
--- routes/model.js
+++ routes/model.js
@@ -4,56 +4,35 @@ var perPage = 5;
 var url = require('url');
 var async = require('async');
 var ObjectID = require('mongodb').ObjectID;
-
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host    :'192.168.10.33',
-    port : 3306,
-    user : 'hadoop',
-    password : 'hadoop',
-    database:'master'
-});
-
-connection.connect(function(err) {
-    if (err) {
-        console.error('mysql connection error');
-        console.error(err);
-        throw err;
-    }
-    console.log("Connected to mysql server");	
-});
-
 //select all
-router.post('/selectType', function(req,res){
+router.post('/selectType', function (req, res) {
+    var mysqlConn = req.mysqlConn;
     var searchword = req.body.searchword
-    //var query = "select * from stopwords where name like concat('%', '" + searchword + "', '%')"
+        //var query = "select * from stopwords where name like concat('%', '" + searchword + "', '%')"
     var query = "select type from chartExample";
     console.log("/route.post /selectType ");
-    var query = connection.query(query,function(err,rows){
-       // console.log(rows);
-	res.jsonp({
-                  "rows": rows
-               });
-
+    var query = mysqlConn.query(query, function (err, rows) {
+        // console.log(rows);
+        res.jsonp({
+            "rows": rows
+        });
     });
     console.log(query);
 });
-
-router.post('/graph', function(req,res){
+router.post('/graph', function (req, res) {
+    var mysqlConn = req.mysqlConn;
     var searchword = req.body.searchword
     console.log("post graph!");
     var query = "select * from chartExample where type='normal'";
     //var query = "select * from chartExample where type= eelike concat('%', '" + searchword + "', '%')"
-    var query = connection.query(query,function(err,rows){
-       // console.log(rows);
-	res.jsonp({
-                  "rows": rows
-               });
-
+    var query = mysqlConn.query(query, function (err, rows) {
+        // console.log(rows);
+        res.jsonp({
+            "rows": rows
+        });
     });
     console.log(query);
 });
-
 router.get('/', ensureAuthenticated, function (req, res, next) {
     res.render('model', {
         "title": 'model'
@@ -61,85 +40,74 @@ router.get('/', ensureAuthenticated, function (req, res, next) {
 });
 
 function ensureAuthenticated(req, res, next) {
-  if(req.isAuthenticated()) {
-    return next();
-  }
-  res.redirect('/users/login');
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/users/login');
 }
-
 router.post('/complete', ensureAuthenticated, function (req, res, next) {
     var selId = req.body.sel_id;
-
     var db = req.db;
     var test_cols = db.get('memo');
     test_cols.update({
-            "_id": selId
-        },
-            {
-                $set: {
-                    "complete": true
-                }
-            },
-            function (err, doc) {
-                if (err) {
-                    throw err;
-                } else {
-                    req.flash('success', 'post completed');
-                    res.jsonp({
-                                    "error_code": 0
-                                });
-                    // res.location('/memo');
-                    // res.redirect('/memo');
-                    // searchHandler(req, res, next);
-                }
-            }
-            );
-
+        "_id": selId
+    }, {
+        $set: {
+            "complete": true
+        }
+    }, function (err, doc) {
+        if (err) {
+            throw err;
+        }
+        else {
+            req.flash('success', 'post completed');
+            res.jsonp({
+                "error_code": 0
+            });
+            // res.location('/memo');
+            // res.redirect('/memo');
+            // searchHandler(req, res, next);
+        }
+    });
 });
-
 router.post('/cancelComplete', ensureAuthenticated, function (req, res, next) {
     var selId = req.body.sel_id;
-
     var db = req.db;
     var test_cols = db.get('contents');
     test_cols.update({
-            "_id": selId
-        },
-            {
-                $set: {
-                    "complete": false
-                }
-            },
-            function (err, doc) {
-                if (err) {
-                    throw err;
-                } else {
-                    req.flash('success', 'Cancel post completion');
-                    res.jsonp({
-                                    "error_code": 0
-                                });
-                    // res.location('/memo');
-                    // res.redirect('/memo');
-                    // searchHandler(req, res, next);
-                }
-            }
-            );
+        "_id": selId
+    }, {
+        $set: {
+            "complete": false
+        }
+    }, function (err, doc) {
+        if (err) {
+            throw err;
+        }
+        else {
+            req.flash('success', 'Cancel post completion');
+            res.jsonp({
+                "error_code": 0
+            });
+            // res.location('/memo');
+            // res.redirect('/memo');
+            // searchHandler(req, res, next);
+        }
+    });
 });
 
 function doJsonSearch(req, res, searchText, searchTags, curPage, completeBool) {
     var db = req.db;
     var mysqlConn = req.mysqlConn;
-    
     var query = "SELECT a.model_id, b1.job_name AS target_job, a.status,";
     query += " b2.job_name AS negative_job, a.model_type, a.model_path, a.reg_date";
     query += " FROM modelMaster a, jobMaster b1, jobMaster b2";
     query += " WHERE a.model_job_id = b1.job_seq AND a.negative_job_id = b2.job_seq";
-    mysqlConn.query(query,function(err,rows){
-    res.jsonp({
-                  "resultDo": rows
-               });
+    mysqlConn.query(query, function (err, rows) {
+        res.jsonp({
+            "resultDo": rows
+        });
     });
-    
     // var test_cols = db.get('contents');
     // var searchQeury;
     // if(searchTags != 'All') {
@@ -157,7 +125,6 @@ function doJsonSearch(req, res, searchText, searchTags, curPage, completeBool) {
     // }
     // // var searchFields = {_id: 1, tags: 1, title: 1, edit_date: 1, contents: 0, reg_date: 0, complete: 0, due_date: 0, notice_bool: 0, reg_id: 0};
     // var searchFields = {contents: 0, reg_date: 0, complete: 0, due_date: 0, notice_bool: 0, reg_id: 0};
-
     // async.parallel([
     //     function(callback) {
     //         test_cols.distinct('tags', {'reg_id': req.user.name}, function (err, categories) {
@@ -185,25 +152,21 @@ function doJsonSearch(req, res, searchText, searchTags, curPage, completeBool) {
     //                     'total_cnt': results[2]
     //                 });
     // });
-
 }
-
 router.post('/search', ensureAuthenticated, function (req, res, next) {
     searchHandler(req, res, next);
 });
-
 router.post('/searchDetail', ensureAuthenticated, function (req, res, next) {
-  var db = req.db;
-  var test_cols = db.get('contents');
-  var selId = new ObjectID(req.body.sel_id);
-
-  test_cols.find({_id: selId},
-      function (err, test_cols) {
-          res.jsonp({
-                          "detailObj": test_cols
-                      });
-      });
-
+    var db = req.db;
+    var test_cols = db.get('contents');
+    var selId = new ObjectID(req.body.sel_id);
+    test_cols.find({
+        _id: selId
+    }, function (err, test_cols) {
+        res.jsonp({
+            "detailObj": test_cols
+        });
+    });
 });
 
 function searchHandler(req, res, next) {
@@ -213,35 +176,32 @@ function searchHandler(req, res, next) {
     var completeBool = req.body.completeBool === undefined ? false : req.body.completeBool;
     doJsonSearch(req, res, searchText, searchTags, curPage, completeBool);
 }
-
 router.post('/savePost', ensureAuthenticated, function (req, res, next) {
     // get form values
     var selContents = req.body.sel_contents;
     var selTitle = req.body.sel_title;
     var selTags = req.body.sel_tags;
     var selId = req.body.sel_id;
-
     var selDueDate = req.body.sel_due_date;
     var selNoticeBool = req.body.sel_notice_bool;
-
     var db = req.db;
     var test_cols = db.get('contents');
-
     if (selId == '') {
         test_cols.insert({
-            "contents": selContents,
-            "title": selTitle,
-            "tags": selTags,
-            "reg_date": new Date(),
-            "edit_date": new Date(),
-            "due_date": selDueDate,
-            "notice_bool": selNoticeBool,
-            "complete" : "n",
-            'reg_id': req.user.name
+            "contents": selContents
+            , "title": selTitle
+            , "tags": selTags
+            , "reg_date": new Date()
+            , "edit_date": new Date()
+            , "due_date": selDueDate
+            , "notice_bool": selNoticeBool
+            , "complete": "n"
+            , 'reg_id': req.user.name
         }, function (err, test_cols) {
             if (err) {
                 res.send('There was an issue submitting the post');
-            } else {
+            }
+            else {
                 // req.flash('success', 'Post Submitted');
                 // res.jsonp({
                 //                 "error_code": 0
@@ -251,38 +211,36 @@ router.post('/savePost', ensureAuthenticated, function (req, res, next) {
                 searchHandler(req, res, next);
             }
         });
-    } else {
+    }
+    else {
         test_cols.update({
             "_id": selId
-        },
-            {
-                $set: {
-                    "contents": selContents,
-                    "title": selTitle,
-                    "tags": selTags,
-                    "reg_date": new Date(),
-                    "edit_date": new Date(),
-                    "due_date": selDueDate,
-                    "notice_bool": selNoticeBool
-                }
-            },
-            function (err, doc) {
-                if (err) {
-                    throw err;
-                } else {
-                    //req.flash('success', 'Comment Added');
-                    // res.jsonp({
-                    //                 "error_code": 0
-                    //             });
-                    // // res.location('/posts/show/'+selId);
-                    // // res.redirect('/posts/show/'+selId);
-                    // res.location('/memo');
-                    // res.redirect('/memo');
-                    searchHandler(req, res, next);
-                }
+        }, {
+            $set: {
+                "contents": selContents
+                , "title": selTitle
+                , "tags": selTags
+                , "reg_date": new Date()
+                , "edit_date": new Date()
+                , "due_date": selDueDate
+                , "notice_bool": selNoticeBool
             }
-            );
+        }, function (err, doc) {
+            if (err) {
+                throw err;
+            }
+            else {
+                //req.flash('success', 'Comment Added');
+                // res.jsonp({
+                //                 "error_code": 0
+                //             });
+                // // res.location('/posts/show/'+selId);
+                // // res.redirect('/posts/show/'+selId);
+                // res.location('/memo');
+                // res.redirect('/memo');
+                searchHandler(req, res, next);
+            }
+        });
     }
 });
-
 module.exports = router;
